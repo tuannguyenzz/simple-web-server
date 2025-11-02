b569b00676f82a20c2a0f3c9a3a65dec48194ea1
tuannguyenzz <136990882+tuannguyenzz@users.noreply.github.com>
2025-11-03 01:03:00 +0700

    Add terraform/outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "region" {
+  value = var.region
+}
+
+output "vpc_id" { value = module.vpc.vpc_id }
+output "public_subnets" { value = module.vpc.public_subnets }
+output "private_subnets" { value = module.vpc.private_subnets }
+
+
+
+
+
+
+
+
+
